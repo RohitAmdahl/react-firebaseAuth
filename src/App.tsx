85578d61153467abd0ad705adf5da816c1b3d484
rohitAmdahl <rohit_designer@outlook.com>
2023-11-19 01:00:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl bg-red-800">Vite + React</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </>
+  );
+}
+
+export default App;

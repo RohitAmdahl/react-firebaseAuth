0a80a153b046c9e7407320601d4035f1b81d5c06
rohitAmdahl <rohit_designer@outlook.com>
2023-11-21 03:06:52 +0100

    setup route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import Layout from "./layout/Layout";
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+const HomePage = lazy(() => import("./pages/Home"));
+const FactPage = lazy(() => import("./pages/Faq"));
+
 function App() {
   return (
     <>
-      <h1 className="text-5xl bg-red-800">Vite + React</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
+      <Suspense fallback={<div> loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/faq" element={<FactPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
